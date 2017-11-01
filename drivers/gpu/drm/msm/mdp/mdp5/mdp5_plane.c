9f02f963e951288f31ceed4aa32f373a79eeacbd
Ville Syrjälä <ville.syrjala@linux.intel.com>
2017-11-01 22:15:58 +0200

    BACKPORT: drm: Check crtc_state->enable rather than crtc->enabled in drm_plane_helper_check_state()
    drm_plane_helper_check_state() is supposed to do things the atomic way,
    so it should not be inspecting crtc->enabled. Rather we should be
    looking at crtc_state->enable.
    
    We have a slight complication due to drm_plane_helper_check_update()
    reusing drm_plane_helper_check_state() for non-atomic drivers. Thus
    we'll have to pass the crtc_state in manally and construct a fake
    crtc_state in drm_plane_helper_check_update().
    
    v2: Fix the WARNs about plane_state->crtc matching crtc_state->crtc
    
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20171101201558.6059-1-ville.syrjala@linux.intel.com
    Reviewed-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    Downstream changes:
    - context difference in mdp5
    - call was missing from vmwgfx
    
    (cherry picked from commit 10b47ee02d1ae66160058241cf5b962f64e81b47)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    Change-Id: I51a7bce98fa84429e39d73fc4c85d870afe35ebe
    Git-commit: 6909e253ec3b2c2115fc3246e3a8f6afb9f0b969
    Git-repo: https://gitlab.freedesktop.org/seanpaul/dpu-staging
    Signed-off-by: Rajesh Yadav <ryadav@codeaurora.org>

--- drivers/gpu/drm/msm/mdp/mdp5/mdp5_plane.c
+++ drivers/gpu/drm/msm/mdp/mdp5/mdp5_plane.c
@@ -327,7 +327,7 @@ static int mdp5_plane_atomic_check_with_state(struct drm_crtc_state *crtc_state,
 	min_scale = FRAC_16_16(1, 8);
 	max_scale = FRAC_16_16(8, 1);
 
-	ret = drm_plane_helper_check_state(state, &clip, min_scale,
+	ret = drm_plane_helper_check_state(state, crtc_state, &clip, min_scale,
 					   max_scale, true, true);
 	if (ret)
 		return ret;
@@ -505,7 +505,7 @@ static int mdp5_plane_atomic_async_check(struct drm_plane *plane,
 	min_scale = FRAC_16_16(1, 8);
 	max_scale = FRAC_16_16(8, 1);
 
-	ret = drm_plane_helper_check_state(state, &clip, min_scale,
+	ret = drm_plane_helper_check_state(state, crtc_state, &clip, min_scale,
 					   max_scale, true, true);
 	if (ret)
 		return ret;
