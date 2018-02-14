12ab2a7eee326a0ba88e560036e8d1723259d814
Sean Paul <seanpaul@chromium.org>
2018-02-14 15:49:40 -0500

    Revert "drm/msm: Fix to pass struct drm_file* to __drm_mode_object_find"
    This reverts commit e65f545f1ad913eeb76fe81974bfe97da9dbdc3d.
    
    Change-Id: I08c7532d09b117f1bd3af9a7e313860986a02b13
    Git-commit: 150865f0d46c02a16a8a16a2cafdc7125d878a3e
    Git-repo: https://gitlab.freedesktop.org/seanpaul/dpu-staging
    Signed-off-by: Rajesh Yadav <ryadav@codeaurora.org>

--- drivers/gpu/drm/msm/msm_drv.c
+++ drivers/gpu/drm/msm/msm_drv.c
@@ -1249,8 +1249,7 @@ static int msm_drm_object_supports_event(struct drm_device *dev,
 	int ret = -EINVAL;
 	struct drm_mode_object *arg_obj;
 
-	arg_obj = drm_mode_object_find(dev, NULL, req->object_id,
-				req->object_type);
+	arg_obj = drm_mode_object_find(dev, req->object_id, req->object_type);
 	if (!arg_obj)
 		return -ENOENT;
 
@@ -1275,8 +1274,7 @@ static int msm_register_event(struct drm_device *dev,
 	struct msm_kms *kms = priv->kms;
 	struct drm_mode_object *arg_obj;
 
-	arg_obj = drm_mode_object_find(dev, NULL, req->object_id,
-				req->object_type);
+	arg_obj = drm_mode_object_find(dev, req->object_id, req->object_type);
 	if (!arg_obj)
 		return -ENOENT;
 
@@ -1557,7 +1555,7 @@ int msm_ioctl_rmfb2(struct drm_device *dev, void *data,
 	if (!drm_core_check_feature(dev, DRIVER_MODESET))
 		return -EINVAL;
 
-	fb = drm_framebuffer_lookup(dev, NULL, *id);
+	fb = drm_framebuffer_lookup(dev, *id);
 	if (!fb)
 		return -ENOENT;
 
