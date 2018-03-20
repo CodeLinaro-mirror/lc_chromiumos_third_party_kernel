b3e467e402b63e8ab2ea58c8ee43e63b560fc8ec
Govind Singh <govinds@codeaurora.org>
2018-03-20 11:52:29 +0530

    FROMLIST: ath10k: Add MSA handshake QMI mgs support
    HOST allocates 2mb of region for modem and WCN3990
    secure access and provides the address and access
    control to target for secure access.
    Add MSA handshake request/response messages.
    
    Cc: ath10k@lists.infradead.org; linux-wireless@vger.kernel.org
    
    Change-Id: I9c4b0923352646fd4a8d56576f97a6166c3f4651
    Signed-off-by: Govind Singh <govinds@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/10307189/)

--- drivers/net/wireless/ath/ath10k/qmi.h
+++ drivers/net/wireless/ath/ath10k/qmi.h
@@ -16,11 +16,19 @@
 #ifndef _QMI_H_
 #define _QMI_H_
 
+#define MAX_NUM_MEMORY_REGIONS			2
+
 enum ath10k_qmi_driver_event_type {
 	ATH10K_QMI_EVENT_SERVER_ARRIVE,
 	ATH10K_QMI_EVENT_SERVER_EXIT,
 	ATH10K_QMI_EVENT_FW_READY_IND,
 	ATH10K_QMI_EVENT_MAX,
+};
+
+struct ath10k_msa_mem_region_info {
+	u64 reg_addr;
+	u32 size;
+	u8 secure_flag;
 };
 
 struct ath10k_qmi {
@@ -33,5 +41,11 @@ struct ath10k_qmi {
 	struct work_struct work_svc_exit;
 	struct workqueue_struct *event_wq;
 	spinlock_t event_lock; /* spinlock for fw ready status*/
+	u32 nr_mem_region;
+	struct ath10k_msa_mem_region_info
+		mem_region[MAX_NUM_MEMORY_REGIONS];
+	phys_addr_t msa_pa;
+	u32 msa_mem_size;
+	void *msa_va;
 };
 #endif /* _QMI_H_ */
