249ee4323beb32b155bd79f2b2326b48f363c3a2
Govind Singh <govinds@codeaurora.org>
2018-05-15 12:24:21 +0530

    INTERNAL: ath10k: set dma mask to 32 bit for wlan
    SMMU s1 stage is disabled for wlan SID due to stale
    dma data seen with LC boot chain. To bypass s1 stage for
    wlan SID, 32 bit dma mask is required.
    
    As temporary workaround, set dma mask to 32 bit for wlan
    SID till S1 stage is enabled
    
    Change-Id: Id4be837176e03436cbd4e2c35e19d3adb68de1e5
    Signed-off-by: Govind Singh <govinds@codeaurora.org>

--- drivers/net/wireless/ath/ath10k/snoc.c
+++ drivers/net/wireless/ath/ath10k/snoc.c
@@ -70,7 +70,7 @@ static void ath10k_snoc_htt_htc_rx_cb(struct ath10k_ce_pipe *ce_state);
 
 static const struct ath10k_snoc_drv_priv drv_priv = {
 	.hw_rev = ATH10K_HW_WCN3990,
-	.dma_mask = DMA_BIT_MASK(37),
+	.dma_mask = DMA_BIT_MASK(32),
 };
 
 #define WCN3990_SRC_WR_INDEX_OFFSET 0x3C
