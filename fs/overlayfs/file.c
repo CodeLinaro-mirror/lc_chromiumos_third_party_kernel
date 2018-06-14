d7b99cd6419dcf3d082c2c4b171bb4ef1b41b359
Mark Salyzyn <salyzyn@android.com>
2018-06-14 11:15:22 -0700

    FROMLIST: overlayfs: override_creds=off option bypass creator_cred
    By default, all access to the upper, lower and work directories is the
    recorded mounter's MAC and DAC credentials.  The incoming accesses are
    checked against the caller's credentials.
    
    If the principles of least privilege are applied, the mounter's
    credentials might not overlap the credentials of the caller's when
    accessing the overlayfs filesystem.  For example, a file that a lower
    DAC privileged caller can execute, is MAC denied to the generally
    higher DAC privileged mounter, to prevent an attack vector.
    
    We add the option to turn off override_creds in the mount options; all
    subsequent operations after mount on the filesystem will be only the
    caller's credentials.  The module boolean parameter and mount option
    override_creds is also added as a presence check for this "feature",
    existence of /sys/module/overlay/parameters/override_creds.
    
    It was not always this way.  Circa 4.6 there was no recorded mounter's
    credentials, instead privileged access to upper or work directories
    were temporarily increased to perform the operations.  The MAC
    (selinux) policies were caller's in all cases.  override_creds=off
    partially returns us to this older access model minus the insecure
    temporary credential increases.  This is to permit use in a system
    with non-overlapping security models for each executable including
    the agent that mounts the overlayfs filesystem.  In Android
    this is the case since init, which performs the mount operations,
    has a minimal MAC set of privileges to reduce any attack surface,
    and services that use the content have a different set of MAC
    privileges (eg: read, for vendor labelled configuration, execute for
    vendor libraries and modules).  The caveats are not a problem in
    the Android usage model, however they should be fixed for
    completeness and for general use in time.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: Miklos Szeredi <miklos@szeredi.hu>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Amir Goldstein <amir73il@gmail.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Cc: linux-unionfs@vger.kernel.org
    Cc: linux-doc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: kernel-team@android.com
    
    (cherry picked from https://lore.kernel.org/lkml/20191104215253.141818-5-salyzyn@android.com/)
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 133515582
    Bug: 136124883
    Bug: 129319403
    Signed-off-by: Greg Kroah-Hartman <gregkh@google.com>
    [fixed up for 5.6-rc1 changes]
    Change-Id: I7980d18e2a5aa97dd2d9fb291fea6deb649f6a34

--- fs/overlayfs/file.c
+++ fs/overlayfs/file.c
@@ -60,7 +60,7 @@ static struct file *ovl_open_realfile(const struct file *file,
 		realfile = open_with_fake_path(&file->f_path, flags, realinode,
 					       current_cred());
 	}
-	revert_creds(old_cred);
+	ovl_revert_creds(inode->i_sb, old_cred);
 
 	pr_debug("open(%p[%pD2/%c], 0%o) -> (%p, 0%o)\n",
 		 file, file, ovl_whatisit(inode, realinode), file->f_flags,
@@ -204,7 +204,7 @@ static loff_t ovl_llseek(struct file *file, loff_t offset, int whence)
 
 	old_cred = ovl_override_creds(inode->i_sb);
 	ret = vfs_llseek(real.file, offset, whence);
-	revert_creds(old_cred);
+	ovl_revert_creds(inode->i_sb, old_cred);
 
 	file->f_pos = real.file->f_pos;
 	ovl_inode_unlock(inode);
@@ -324,7 +324,8 @@ static ssize_t ovl_read_iter(struct kiocb *iocb, struct iov_iter *iter)
 			ovl_aio_cleanup_handler(aio_req);
 	}
 out:
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file)->i_sb, old_cred);
+
 	ovl_file_accessed(file);
 out_fdput:
 	fdput(real);
@@ -395,7 +396,7 @@ static ssize_t ovl_write_iter(struct kiocb *iocb, struct iov_iter *iter)
 			ovl_aio_cleanup_handler(aio_req);
 	}
 out:
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file)->i_sb, old_cred);
 out_fdput:
 	fdput(real);
 
@@ -441,7 +442,7 @@ static ssize_t ovl_splice_write(struct pipe_inode_info *pipe, struct file *out,
 	file_end_write(real.file);
 	/* Update size */
 	ovl_copyattr(realinode, inode);
-	revert_creds(old_cred);
+	ovl_revert_creds(inode->i_sb, old_cred);
 	fdput(real);
 
 out_unlock:
@@ -468,7 +469,7 @@ static int ovl_fsync(struct file *file, loff_t start, loff_t end, int datasync)
 	if (file_inode(real.file) == ovl_inode_upper(file_inode(file))) {
 		old_cred = ovl_override_creds(file_inode(file)->i_sb);
 		ret = vfs_fsync_range(real.file, start, end, datasync);
-		revert_creds(old_cred);
+		ovl_revert_creds(file_inode(file)->i_sb, old_cred);
 	}
 
 	fdput(real);
@@ -492,7 +493,7 @@ static int ovl_mmap(struct file *file, struct vm_area_struct *vma)
 
 	old_cred = ovl_override_creds(file_inode(file)->i_sb);
 	ret = call_mmap(vma->vm_file, vma);
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file)->i_sb, old_cred);
 	ovl_file_accessed(file);
 
 	return ret;
@@ -511,7 +512,7 @@ static long ovl_fallocate(struct file *file, int mode, loff_t offset, loff_t len
 
 	old_cred = ovl_override_creds(file_inode(file)->i_sb);
 	ret = vfs_fallocate(real.file, mode, offset, len);
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file)->i_sb, old_cred);
 
 	/* Update size */
 	ovl_copyattr(ovl_inode_real(inode), inode);
@@ -533,7 +534,7 @@ static int ovl_fadvise(struct file *file, loff_t offset, loff_t len, int advice)
 
 	old_cred = ovl_override_creds(file_inode(file)->i_sb);
 	ret = vfs_fadvise(real.file, offset, len, advice);
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file)->i_sb, old_cred);
 
 	fdput(real);
 
@@ -583,7 +584,7 @@ static loff_t ovl_copyfile(struct file *file_in, loff_t pos_in,
 						flags);
 		break;
 	}
-	revert_creds(old_cred);
+	ovl_revert_creds(file_inode(file_out)->i_sb, old_cred);
 
 	/* Update size */
 	ovl_copyattr(ovl_inode_real(inode_out), inode_out);
