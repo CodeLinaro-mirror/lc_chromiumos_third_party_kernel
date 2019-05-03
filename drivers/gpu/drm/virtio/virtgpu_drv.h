23698509d068fed41a4ea9bbe8dd9378ebe169a4
Gurchetan Singh <gurchetansingh@chromium.org>
2019-05-03 08:42:53 -0700

    CHROMIUM: drm/virtgpu: add stub ioctl implementation
    Add plumbing for new ioctls.
    
    BUG=chromium:924405
    TEST=compile
    
    Change-Id: Idf42b6e1b1bbf4b753b8ff0ac8c5f65dc21ec27e
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1629911
    Reviewed-by: Robert Tarasov <tutankhamen@chromium.org>
    Commit-Queue: Gurchetan Singh <gurchetansingh@chromium.org>
    Tested-by: Gurchetan Singh <gurchetansingh@chromium.org>
    [rebase54(groeck): Context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/gpu/drm/virtio/virtgpu_drv.h
+++ drivers/gpu/drm/virtio/virtgpu_drv.h
@@ -233,7 +233,7 @@ struct virtio_gpu_fpriv {
 };
 
 /* virtio_ioctl.c */
-#define DRM_VIRTIO_NUM_IOCTLS 10
+#define DRM_VIRTIO_NUM_IOCTLS 13
 extern struct drm_ioctl_desc virtio_gpu_ioctls[DRM_VIRTIO_NUM_IOCTLS];
 void virtio_gpu_create_context(struct drm_device *dev, struct drm_file *file);
 
