16ded1b9e90fddbb41a5fe34486345bc26c64211
Alain Michaud <alainmichaud@google.com>
2020-03-13 16:25:26 -0400

    CHROMIUM: bluetooth: Aligning mgmt op_codes to upstream.
    This change aligns the mgmt op_codes to upstream and leaves a gap for
    future expandability with minimal merge conflicts.
    
    BUG=b:146642641
    TEST=Built
    
    Signed-off-by: Alain Michaud <alainm@chromium.org>
    Cq-Depend: chromium:2104527
    Change-Id: I424841275f1e96cdba0f031414c9b7b484961603
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2104533
    Reviewed-by: Michael Sun <michaelfsun@google.com>

--- include/net/bluetooth/mgmt.h
+++ include/net/bluetooth/mgmt.h
@@ -605,14 +605,17 @@ struct mgmt_cp_set_appearance {
 } __packed;
 #define MGMT_SET_APPEARANCE_SIZE	2
 
-#define MGMT_OP_SET_ADVERTISING_INTERVALS	0x0044
+/*
+ * Begin chromium only op_codes
+ */
+#define MGMT_OP_SET_ADVERTISING_INTERVALS	0x0060
 struct mgmt_cp_set_advertising_intervals {
 	__le16	min_interval;
 	__le16	max_interval;
 } __packed;
 #define MGMT_SET_ADVERTISING_INTERVALS_SIZE	4
 
-#define MGMT_OP_SET_EVENT_MASK			0x0045
+#define MGMT_OP_SET_EVENT_MASK			0x0061
 struct mgmt_cp_set_event_mask {
 	/*
 	 * The mask variable enables modifying a subset of the 'event mask'.
@@ -688,23 +691,27 @@ enum mgmt_set_event_mask_byte_7 {
 	MGMT_EVENT_MASK_LE_META				=  (1 << 6),
 };
 
-#define MGMT_OP_SET_BLOCKED_LTKS			0x0046
+#define MGMT_OP_SET_BLOCKED_LTKS			0x0062
 struct mgmt_cp_set_blocked_ltks {
 	uint8_t	ltks[MAX_BLOCKED_LTKS][LTK_LENGTH];
 } __packed;
 #define MGMT_SET_BLOCKED_LTKS_CP_SIZE  (MAX_BLOCKED_LTKS * LTK_LENGTH)
 
-#define MGMT_OP_READ_SUPPORTED_CAPABILITIES		0x0047
+#define MGMT_OP_READ_SUPPORTED_CAPABILITIES		0x0063
 #define MGMT_READ_SUPPORTED_CAPABILITIES_SIZE		0
 struct mgmt_rp_read_supported_capabilities {
 	__u8 wide_band_speech;
 } __packed;
 
-#define MGMT_OP_SET_KERNEL_DEBUG			0x0048
+#define MGMT_OP_SET_KERNEL_DEBUG			0x0064
 #define MGMT_SET_KERNEL_DEBUG_SIZE			1
 struct mgmt_cp_set_kernel_debug {
 	__u8	enabled;
 } __packed;
+
+/*
+ * End chromium only op_codes
+ */
 
 #define MGMT_EV_CMD_COMPLETE		0x0001
 struct mgmt_ev_cmd_complete {
