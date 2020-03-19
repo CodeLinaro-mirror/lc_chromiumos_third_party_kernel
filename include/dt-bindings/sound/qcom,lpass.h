7b736b4c0c122791de1f00b8b9e35eaf11319eba
Ajit Pandey <ajitp@codeaurora.org>
2020-03-19 19:50:35 +0530

    CHROMIUM: dt-bindings: sound: Add qcom common lpass cpu driver bindings
    BUG=b:158052905
    TEST=test audio with whole patchset
    
    Change-Id: I510739fd36b5db843bbd530d37eb7a4b5fd0a3a1
    Signed-off-by: Ajit Pandey <ajitp@codeaurora.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2241439
    Reviewed-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Commit-Queue: Cheng-Yi Chiang <cychiang@chromium.org>
    Tested-by: Cheng-Yi Chiang <cychiang@chromium.org>

---
+++ include/dt-bindings/sound/qcom,lpass.h
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: GPL-2.0-only
+/*
+ * Copyright (c) 2020, The Linux Foundation. All rights reserved.
+ *
+ * lpass.h -- DT bindings properties related to LPASS MI2S dai's
+ */
+
+#ifndef _DT_BINDINGS_LPASS_H
+#define _DT_BINDINGS_LPASS_H
+
+#define MI2S_PRIMARY	0
+#define MI2S_SECONDARY	1
+#define MI2S_TERTIARY	2
+#define MI2S_QUATERNARY	3
+
+/* ipq806x specific */
+#define IPQ806X_LPAIF_I2S_PORT_MI2S 4
+
+#endif /* __DT_BINDINGS_LPASS_H__ */
