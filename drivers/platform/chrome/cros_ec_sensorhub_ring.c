56180992e7f55d76215e7876ddab5d6c3a3f9db9
Lukasz Majczak <lma@semihalf.com>
2020-04-01 20:20:51 +0200

    rebase56(lma):
    	* remove lefotover of linux/mfd/cros_ec.h usage
    	* fix function name

--- drivers/platform/chrome/cros_ec_sensorhub_ring.c
+++ drivers/platform/chrome/cros_ec_sensorhub_ring.c
@@ -9,7 +9,6 @@
 #include <linux/device.h>
 #include <linux/iio/iio.h>
 #include <linux/kernel.h>
-#include <linux/mfd/cros_ec.h>
 #include <linux/module.h>
 #include <linux/platform_data/cros_ec_commands.h>
 #include <linux/platform_data/cros_ec_proto.h>
