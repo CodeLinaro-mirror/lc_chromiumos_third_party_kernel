3e2ea7efd69f43af0bf0032af9236279f1f2976e
Abhinav Kumar <abhinavk@codeaurora.org>
2020-09-10 19:07:34 +0000

    FROMLIST: drm/msm/dp: add audio support for Display Port on MSM
    Introduce audio support for Display Port on MSM chipsets.
    This change integrates DP audio sub-module with the main
    Display Port platform driver.
    
    In addition, this change leverages hdmi_codec_ops to expose
    the operations to the audio driver.
    
    Changes in v2: fix up a compilation issue on drm-next branch
    Changes in v3: none
    Changes in v4: none
    Changes in v5: none
    
    Signed-off-by: Abhinav Kumar <abhinavk@codeaurora.org>
    (am from https://patchwork.kernel.org/patch/11768875/)
    (also found at https://lore.kernel.org/r/01010174796a76a3-0094092f-5f9e-4e95-8f91-ddd3d59024a3-000000@us-west-2.amazonses.com)
    
    BUG=b:147399356 b:161530390
    TEST=check DP audio on lazor
    
    Signed-off-by: Cheng-Yi Chiang <cychiang@chromium.org>
    Change-Id: I09c85d0b6ad5c190215256d3d8317c66a94be371
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2409827

--- drivers/gpu/drm/msm/dp/dp_display.c
+++ drivers/gpu/drm/msm/dp/dp_display.c
@@ -25,6 +25,7 @@
 #include "dp_display.h"
 #include "dp_drm.h"
 #include "dp_pll.h"
+#include "dp_audio.h"
 
 static struct msm_dp *g_dp_display;
 #define HPD_STRING_SIZE 30
@@ -105,6 +106,8 @@ struct dp_display_private {
 	u32 event_gndx;
 	struct dp_event event_list[DP_EVENT_Q_MAX];
 	spinlock_t event_lock;
+
+	struct dp_audio *audio;
 };
 
 static const struct of_device_id dp_dt_match[] = {
@@ -204,6 +207,13 @@ static int dp_display_bind(struct device *dev, struct device *master,
 		DRM_ERROR("Power client create failed\n");
 		goto end;
 	}
+
+	rc = dp_register_audio_driver(dev, dp->audio);
+	if (rc) {
+		DRM_ERROR("Audio registration Dp failed\n");
+		goto end;
+	}
+
 end:
 	return rc;
 }
@@ -376,8 +386,19 @@ static int dp_display_usbpd_disconnect_cb(struct device *dev)
 	int rc = 0;
 	struct dp_display_private *dp;
 
+	if (!dev) {
+		DRM_ERROR("invalid dev\n");
+		rc = -EINVAL;
+		return rc;
+	}
+
 	dp = container_of(g_dp_display,
 			struct dp_display_private, dp_display);
+	if (!dp) {
+		DRM_ERROR("no driver data found\n");
+		rc = -ENODEV;
+		return rc;
+	}
 
 	dp_add_event(dp, EV_USER_NOTIFICATION, false, 0);
 
@@ -608,6 +629,7 @@ static void dp_display_deinit_sub_modules(struct dp_display_private *dp)
 	dp_panel_put(dp->panel);
 	dp_aux_put(dp->aux);
 	dp_pll_put(dp->pll);
+	dp_audio_put(dp->audio);
 }
 
 static int dp_init_sub_modules(struct dp_display_private *dp)
@@ -707,7 +729,18 @@ static int dp_init_sub_modules(struct dp_display_private *dp)
 		goto error_ctrl;
 	}
 
+	dp->audio = dp_audio_get(dp->pdev, dp->panel, dp->catalog);
+	if (IS_ERR(dp->audio)) {
+		rc = PTR_ERR(dp->audio);
+		pr_err("failed to initialize audio, rc = %d\n", rc);
+		dp->audio = NULL;
+		goto error_audio;
+	}
+
 	return rc;
+
+error_audio:
+	dp_ctrl_put(dp->ctrl);
 error_ctrl:
 	dp_panel_put(dp->panel);
 error_link:
@@ -753,6 +786,18 @@ static int dp_display_enable(struct dp_display_private *dp, u32 data)
 
 static int dp_display_post_enable(struct msm_dp *dp_display)
 {
+	struct dp_display_private *dp;
+	u32 rate;
+
+	dp = container_of(dp_display, struct dp_display_private, dp_display);
+
+	rate = dp->link->link_params.rate;
+
+	if (dp->audio_supported) {
+		dp->audio->bw_code = drm_dp_link_rate_to_bw_code(rate);
+		dp->audio->lane_count = dp->link->link_params.num_lanes;
+	}
+
 	return 0;
 }
 
@@ -1059,6 +1104,9 @@ static int dp_display_probe(struct platform_device *pdev)
 	mutex_init(&dp->event_mutex);
 
 	g_dp_display = &dp->dp_display;
+
+	/* Store DP audio handle inside DP display */
+	g_dp_display->dp_audio = dp->audio;
 
 	platform_set_drvdata(pdev, g_dp_display);
 
