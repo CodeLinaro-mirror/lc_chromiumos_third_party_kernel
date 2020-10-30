34aa993b18755e34da2d258773dc705ab0116d9a
Wei Wang <wvw@google.com>
2020-10-29 22:49:45 -0700

    ANDROID: sched: Add vendor hooks for skipping sugov update
    This is to add capability for vendor to decide whether a cpufreq update
    is needed, e.g. up/down rate limit.
    
    Using restricted hook since it can be called from scheduler wakeup path.
    
    Bug: 170511085
    Signed-off-by: Wei Wang <wvw@google.com>
    Change-Id: If9adea3a3e31efbf3858fbd009665a07dc70c638
    (cherry picked from commit f9f3464532a045257f8138338b1beda86ef0a3be)
    Signed-off-by: Will McVicker <willmcvicker@google.com>

--- include/trace/hooks/sched.h
+++ include/trace/hooks/sched.h
@@ -112,6 +112,10 @@ DECLARE_HOOK(android_vh_set_sugov_sched_attr,
 DECLARE_RESTRICTED_HOOK(android_rvh_set_iowait,
 	TP_PROTO(struct task_struct *p, int *should_iowait_boost),
 	TP_ARGS(p, should_iowait_boost), 1);
+struct sugov_policy;
+DECLARE_RESTRICTED_HOOK(android_rvh_set_sugov_update,
+	TP_PROTO(struct sugov_policy *sg_policy, unsigned int next_freq, bool *should_update),
+	TP_ARGS(sg_policy, next_freq, should_update), 1);
 DECLARE_RESTRICTED_HOOK(android_rvh_sched_setaffinity,
 	TP_PROTO(struct task_struct *p, const struct cpumask *in_mask, int *retval),
 	TP_ARGS(p, in_mask, retval), 1);
@@ -155,6 +159,7 @@ DECLARE_HOOK(android_vh_em_cpu_energy,
 #define trace_android_rvh_find_energy_efficient_cpu(p, prev_cpu, sync, new_cpu)
 #define trace_android_vh_set_sugov_sched_attr(attr)
 #define trace_android_rvh_set_iowait(p, should_iowait_boost)
+#define trace_android_rvh_set_sugov_update(sg_policy, next_freq, should_update)
 #define trace_android_rvh_sched_setaffinity(p, in_mask, retval)
 #define trace_android_rvh_cpu_cgroup_attach(tset)
 #endif
