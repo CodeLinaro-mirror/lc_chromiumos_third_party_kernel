946aba45ce779895d0fc1fca38ec14c943f0d2e0
Kevin Chowski <chowski@chromium.org>
2020-11-30 17:39:40 -0700

    CHROMIUM: nvme: disable the write zeros command for Pixelbook.
    This is a followup to a suggestion made in https://crbug.com/1141713#c68
    
    This is a chromium-only patch because we aim to test the theory that
    the "Write Zeros" command is causing a problem on Pixelbooks in our lab.
    The evidence still seems a little too circumstantial at the moment
    (to me) to proactively do the leg work to get it upstream given
    there's a risk that we will want to revert if the issue remains, but if
    it turns out to mitigate the issue then for sure I will revert and
    get it in upstream instead. If you think it's better off going upstream
    in the first place please let me know.
    
    I retrieved the device info from:
    http://cs/chromeos_internal/src/platform/chromeos-hwid/v3/EVE
    
    BUG=b:174236852
    TEST=just compiled (emerge-eve-kernelnext chromeos-kernel-5_4)
    
    Change-Id: Iefe61257340139c157e34bfec2e885a19ee7a0e8
    Signed-off-by: Kevin Chowski <chowski@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2566697
    Tested-by: Kevin Chowski <chowski@google.com>
    Reviewed-by: Gwendal Grignou <gwendal@chromium.org>
    Commit-Queue: Kevin Chowski <chowski@google.com>
    Auto-Submit: Kevin Chowski <chowski@google.com>
    Conflicts:
    	drivers/nvme/host/pci.c
    [rebase510(groeck): Context conflicts; moved quirk to match
    	other Samsung drives]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>

--- drivers/nvme/host/pci.c
+++ drivers/nvme/host/pci.c
@@ -3259,6 +3259,8 @@ static const struct pci_device_id nvme_id_table[] = {
 		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x1d97, 0x2263),   /* SPCC */
 		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
+	{ PCI_DEVICE(PCI_VENDOR_ID_SAMSUNG, 0xa806),   /* Samsung KUS040205M-B001 */
+		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x144d, 0xa809),   /* Samsung 128HBHQ and 256HAJD */
 		.driver_data = NVME_QUIRK_DISABLE_WRITE_ZEROES, },
 	{ PCI_DEVICE(0x1987, 0x5013),   /* Phison PS5013 E13 */
