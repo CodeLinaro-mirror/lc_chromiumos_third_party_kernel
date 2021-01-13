ec322abef35615026435c68e404d01efca90c50f
Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
2021-01-13 10:49:33 +0530

    iommu/arm-smmu-qcom: Add sc7280 smmu compatible
    Add compatible for SC7280 SMMU to use the Qualcomm Technologies, Inc.
    specific implementation.
    
    Signed-off-by: Sai Prakash Ranjan <saiprakash.ranjan@codeaurora.org>
    Change-Id: I0a32454db2bc15f4dfa1b4664fde1f6cce3d17d4

--- drivers/iommu/arm/arm-smmu/arm-smmu-impl.c
+++ drivers/iommu/arm/arm-smmu/arm-smmu-impl.c
@@ -11,6 +11,7 @@
 
 static const struct of_device_id __maybe_unused qcom_smmu_impl_of_match[] = {
 	{ .compatible = "qcom,sc7180-smmu-500" },
+	{ .compatible = "qcom,sc7280-smmu-500" },
 	{ .compatible = "qcom,sdm845-smmu-500" },
 	{ .compatible = "qcom,sm8150-smmu-500" },
 	{ .compatible = "qcom,sm8250-smmu-500" },
