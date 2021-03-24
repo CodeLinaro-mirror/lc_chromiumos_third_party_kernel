652a5d31ee4547cd3671e714f4ba3e0f9d92fc2e
Aubrey Li <aubrey.li@linux.intel.com>
2021-03-24 17:40:13 -0400

    FROMLIST: sched: Migration changes for core scheduling
     - Don't migrate if there is a cookie mismatch
         Load balance tries to move task from busiest CPU to the
         destination CPU. When core scheduling is enabled, if the
         task's cookie does not match with the destination CPU's
         core cookie, this task may be skipped by this CPU. This
         mitigates the forced idle time on the destination CPU.
    
     - Select cookie matched idle CPU
         In the fast path of task wakeup, select the first cookie matched
         idle CPU instead of the first idle CPU.
    
     - Find cookie matched idlest CPU
         In the slow path of task wakeup, find the idlest CPU whose core
         cookie matches with task's cookie
    
    (am from
    https://lore.kernel.org/lkml/20210422123308.860083871@infradead.org/#t)
    
    BUG=b:152605392
    TEST=run power_VideoCall test
    
    Signed-off-by: Aubrey Li <aubrey.li@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Tested-by: Don Hiatt <dhiatt@digitalocean.com>
    Signed-off-by: Joel Fernandes <joelaf@google.com>
    Change-Id: I65d0e47c708d892b4820edafd9e0ea3536ba2ab4
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2880790
    Reviewed-by: Sonny Rao <sonnyrao@chromium.org>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -5889,11 +5889,15 @@ find_idlest_group_cpu(struct sched_group *group, struct task_struct *p, int this
 
 	/* Traverse only the allowed CPUs */
 	for_each_cpu_and(i, sched_group_span(group), p->cpus_ptr) {
+		struct rq *rq = cpu_rq(i);
+
+		if (!sched_core_cookie_match(rq, p))
+			continue;
+
 		if (sched_idle_cpu(i))
 			return i;
 
 		if (available_idle_cpu(i)) {
-			struct rq *rq = cpu_rq(i);
 			struct cpuidle_state *idle = idle_get_state(rq);
 			if (idle && idle->exit_latency < min_exit_latency) {
 				/*
@@ -6054,7 +6058,8 @@ static int select_idle_core(struct task_struct *p, int core, struct cpumask *cpu
 		return __select_idle_cpu(core);
 
 	for_each_cpu(cpu, cpu_smt_mask(core)) {
-		if (!available_idle_cpu(cpu)) {
+		if (!available_idle_cpu(cpu) ||
+				!sched_cpu_cookie_match(cpu_rq(cpu), p)) {
 			idle = false;
 			if (*idle_cpu == -1) {
 				if (sched_idle_cpu(cpu) && cpumask_test_cpu(cpu, p->cpus_ptr)) {
@@ -7521,6 +7526,14 @@ static int task_hot(struct task_struct *p, struct lb_env *env)
 
 	if (sysctl_sched_migration_cost == -1)
 		return 1;
+
+	/*
+	 * Don't migrate task if the task's cookie does not match
+	 * with the destination CPU's core cookie.
+	 */
+	if (!sched_core_cookie_match(cpu_rq(env->dst_cpu), p))
+		return 1;
+
 	if (sysctl_sched_migration_cost == 0)
 		return 0;
 
@@ -8849,6 +8862,10 @@ find_idlest_group(struct sched_domain *sd, struct task_struct *p, int this_cpu)
 		/* Skip over this group if it has no CPUs allowed */
 		if (!cpumask_intersects(sched_group_span(group),
 					p->cpus_ptr))
+			continue;
+
+		/* Skip over this group if no cookie matched */
+		if (!sched_group_cookie_match(cpu_rq(this_cpu), p, group))
 			continue;
 
 		local_group = cpumask_test_cpu(this_cpu,
