e6410e9d26293fa4ae249a60da84575799686a02
José Roberto de Souza <jose.souza@intel.com>
2021-04-08 13:31:50 -0700

    FROMGIT: drm/i915: skip display initialization when there is no display
    Display features should not be initialized or de-initialized when there
    is no display. Skip modeset initialization, output setup, plane, crtc,
    encoder, connector registration, display cdclk and rawclk
    initialization, display core initialization, etc.
    
    Skip the functionality at as high level as possible, and remove any
    redundant checks. If the functionality is conditional to *other* display
    checks, do not add more. If the un-initialization has checks for
    initialization, do not add more.
    
    We explicitly do not care about any GMCH/VLV/CHV code paths, as they've
    always had and will have display.
    
    Reviewed-by: Radhakrishna Sripada <radhakrishna.sripada@intel.com>
    Cc: Lucas De Marchi <lucas.demarchi@intel.com>
    Signed-off-by: José Roberto de Souza <jose.souza@intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Signed-off-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20210408203150.237947-3-jose.souza@intel.com
    (cherry picked from commit 5df7bd130818dfdc9047c2a81b19737d66e55f9a
     git://anongit.freedesktop.org/drm-intel drm-intel-next)
    Signed-off-by: Sean Paul <seanpaul@chromium.org>
    
    BUG=b:187093397
    TEST=Tested with single/multiple displays, suspend/resume, hotplugs on volteer/trogdor/dedede/zork, build tested on amd64/arm64-generic
    
    Change-Id: Ie2f7ab8dd3d0cc83f01bf116eb6bb9e472e8c696
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2889110
    Reviewed-by: Sean Paul <seanpaul@chromium.org>
    Reviewed-by: Alex Levin <levinale@google.com>
    Tested-by: Sean Paul <seanpaul@chromium.org>
    Commit-Queue: Sean Paul <seanpaul@chromium.org>

--- drivers/gpu/drm/i915/display/intel_dp.c
+++ drivers/gpu/drm/i915/display/intel_dp.c
@@ -5393,6 +5393,9 @@ void intel_dp_mst_suspend(struct drm_i915_private *dev_priv)
 {
 	struct intel_encoder *encoder;
 
+	if (!HAS_DISPLAY(dev_priv))
+		return;
+
 	for_each_intel_encoder(&dev_priv->drm, encoder) {
 		struct intel_dp *intel_dp;
 
@@ -5412,6 +5415,9 @@ void intel_dp_mst_suspend(struct drm_i915_private *dev_priv)
 void intel_dp_mst_resume(struct drm_i915_private *dev_priv)
 {
 	struct intel_encoder *encoder;
+
+	if (!HAS_DISPLAY(dev_priv))
+		return;
 
 	for_each_intel_encoder(&dev_priv->drm, encoder) {
 		struct intel_dp *intel_dp;
