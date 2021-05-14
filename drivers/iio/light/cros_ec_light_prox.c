a3cd215e04ea82f5692a4346ea045da9d0637836
Google Cont Uprev <goog-uprev-dev@semihalf.com>
2021-05-14 15:50:23 +0200

    FIXUP: Add fixup for cros_ec due to upstream API change
    Signed-off-by: Google Cont Uprev <goog-uprev-dev@semihalf.com>

--- drivers/iio/light/cros_ec_light_prox.c
+++ drivers/iio/light/cros_ec_light_prox.c
@@ -422,8 +422,7 @@ static int cros_ec_light_prox_probe(struct platform_device *pdev)
 
 	ret = cros_ec_sensors_core_init(pdev, indio_dev, true,
 					cros_ec_light_capture,
-					cros_ec_light_push_data,
-					true);
+					cros_ec_light_push_data);
 	if (ret)
 		return ret;
 
