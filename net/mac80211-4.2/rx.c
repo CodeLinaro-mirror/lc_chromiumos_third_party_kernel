075e26f3a07f304b3c2a0384073692c59d4c99cd
ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>
2021-06-16 13:14:57 +0000

    Revert "CHROMIUM: mac80211: check defrag PN against current frame"
    This reverts commit 155a47a32141b887de6b5362a9a1d2aa5aa4a66d.
    
    Reason for revert: fixing compilation issues
    
    Original change's description:
    > CHROMIUM: mac80211: check defrag PN against current frame
    >
    > As pointed out by Mathy Vanhoef, we implement the RX PN check
    > on fragmented frames incorrectly - we check against the last
    > received PN prior to the new frame, rather than to the one in
    > this frame itself.
    >
    > Prior patches addressed the security issue here, but in order
    > to be able to reason better about the code, fix it to really
    > compare against the current frame's PN, not the last stored
    > one.
    >
    > BUG=b:179418147
    > TEST=manual testing on G
    >
    > Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    > Signed-off-by: vadiraj kakhandaki <vadirajk@google.com>
    > Change-Id: I3f274e822fc581b41594849b16433f5ddd43a8cd
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2914919
    > Reviewed-by: Julan Hsu <julanhsu@google.com>
    > Reviewed-by: Maharaja Kennadyrajan <mkenna@qualcomm.corp-partner.google.com>
    > (cherry picked from commit b34d2ef43ef617879d4f4f086d0b2b8fc3252ab9)
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2960553
    > Tested-by: ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>
    > Commit-Queue: Julan Hsu <julanhsu@google.com>
    
    Bug: b:179418147
    Change-Id: I74ecf8344f625ef64307eae7b93928d432395d87
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2966176
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>

--- net/mac80211-4.2/rx.c
+++ net/mac80211-4.2/rx.c
@@ -1917,6 +1917,7 @@ ieee80211_rx_h_defragment(struct ieee80211_rx_data *rx)
 	if (entry->ccmp) {
 		int i;
 		u8 pn[IEEE80211_CCMP_PN_LEN], *rpn;
+		int queue;
 
 		if (!requires_sequential_pn(rx, fc))
 			return RX_DROP_UNUSABLE;
@@ -1931,8 +1932,8 @@ ieee80211_rx_h_defragment(struct ieee80211_rx_data *rx)
 			if (pn[i])
 				break;
 		}
-
-		rpn = rx->ccm_gcm.pn;
+		queue = rx->security_idx;
+		rpn = rx->key->u.ccmp.rx_pn[queue];
 		if (memcmp(pn, rpn, IEEE80211_CCMP_PN_LEN))
 			return RX_DROP_UNUSABLE;
 		memcpy(entry->last_pn, pn, IEEE80211_CCMP_PN_LEN);
