2794301585e0e9d1bb94c41548e94255a1852733
ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>
2021-06-17 00:05:19 +0000

    Revert "CHROMIUM: mac80211: assure all fragments are encrypted"
    This reverts commit c19052c9ba990bcc1cf687c430e6de4a431df6dd.
    
    Reason for revert: fix build errors
    
    Original change's description:
    > CHROMIUM: mac80211: assure all fragments are encrypted
    >
    > Do not mix plaintext and encrypted fragments in protected Wi-Fi
    > networks. This fixes CVE-2020-26147.
    >
    > Previously, an attacker was able to first forward a legitimate encrypted
    > fragment towards a victim, followed by a plaintext fragment. The
    > encrypted and plaintext fragment would then be reassembled. For further
    > details see Section 6.3 and Appendix D in the paper "Fragment and Forge:
    > Breaking Wi-Fi Through Frame Aggregation and Fragmentation".
    >
    > Because of this change there are now two equivalent conditions in the
    > code to determine if a received fragment requires sequential PNs, so we
    > also move this test to a separate function to make the code easier to
    > maintain.
    >
    > BUG=b:179418220
    > TEST=manual testing on M
    >
    > Signed-off-by: Mathy Vanhoef <Mathy.Vanhoef@kuleuven.be>
    > Signed-off-by: Johannes Berg <johannes.berg@intel.com>
    > Signed-off-by: vadiraj kakhandaki <vadirajk@google.com>
    > Change-Id: I523641846892283a33a9917a5a066ab2b1f37275
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2921856
    > Tested-by: ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>
    > Reviewed-by: Balaji Pothunoori <bpothuno%qti.qualcomm.com@gtempaccount.com>
    > Reviewed-by: Julan Hsu <julanhsu@google.com>
    > Reviewed-by: Maharaja Kennadyrajan <mkenna@qualcomm.corp-partner.google.com>
    > (cherry picked from commit 8213f79d6101cc81ff5390bb4ff0a4946d7e7097)
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2960692
    > Commit-Queue: Julan Hsu <julanhsu@google.com>
    
    Bug: b:179418220
    Change-Id: Ia0a14e7cb7c37359a8027e1695d3a8d811bf02e5
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2966680
    Bot-Commit: Rubber Stamper <rubber-stamper@appspot.gserviceaccount.com>
    Commit-Queue: ANKIT DARSI AMARNATHAGUPTHA <dankit@google.com>

--- net/mac80211/rx.c
+++ net/mac80211/rx.c
@@ -1973,16 +1973,6 @@ ieee80211_reassemble_find(struct ieee80211_sub_if_data *sdata,
 	return NULL;
 }
 
-static bool requires_sequential_pn(struct ieee80211_rx_data *rx, __le16 fc)
-{
-	return rx->key &&
-		(rx->key->conf.cipher == WLAN_CIPHER_SUITE_CCMP ||
-		 rx->key->conf.cipher == WLAN_CIPHER_SUITE_CCMP_256 ||
-		 rx->key->conf.cipher == WLAN_CIPHER_SUITE_GCMP ||
-		 rx->key->conf.cipher == WLAN_CIPHER_SUITE_GCMP_256) &&
-		ieee80211_has_protected(fc);
-}
-
 static ieee80211_rx_result debug_noinline
 ieee80211_rx_h_defragment(struct ieee80211_rx_data *rx)
 {
@@ -2027,7 +2017,12 @@ ieee80211_rx_h_defragment(struct ieee80211_rx_data *rx)
 		/* This is the first fragment of a new frame. */
 		entry = ieee80211_reassemble_add(rx->sdata, frag, seq,
 						 rx->seqno_idx, &(rx->skb));
-		if (requires_sequential_pn(rx, fc)) {
+		if (rx->key &&
+		    (rx->key->conf.cipher == WLAN_CIPHER_SUITE_CCMP ||
+		     rx->key->conf.cipher == WLAN_CIPHER_SUITE_CCMP_256 ||
+		     rx->key->conf.cipher == WLAN_CIPHER_SUITE_GCMP ||
+		     rx->key->conf.cipher == WLAN_CIPHER_SUITE_GCMP_256) &&
+		    ieee80211_has_protected(fc)) {
 			int queue = rx->security_idx;
 
 			/* Store CCMP/GCMP PN so that we can verify that the
@@ -2069,7 +2064,11 @@ ieee80211_rx_h_defragment(struct ieee80211_rx_data *rx)
 		u8 pn[IEEE80211_CCMP_PN_LEN], *rpn;
 		int queue;
 
-		if (!requires_sequential_pn(rx, fc))
+		if (!rx->key ||
+		    (rx->key->conf.cipher != WLAN_CIPHER_SUITE_CCMP &&
+		     rx->key->conf.cipher != WLAN_CIPHER_SUITE_CCMP_256 &&
+		     rx->key->conf.cipher != WLAN_CIPHER_SUITE_GCMP &&
+		     rx->key->conf.cipher != WLAN_CIPHER_SUITE_GCMP_256))
 			return RX_DROP_UNUSABLE;
 		memcpy(pn, entry->last_pn, IEEE80211_CCMP_PN_LEN);
 		for (i = IEEE80211_CCMP_PN_LEN - 1; i >= 0; i--) {
