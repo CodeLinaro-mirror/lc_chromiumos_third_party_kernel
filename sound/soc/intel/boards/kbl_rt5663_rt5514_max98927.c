452a507db5dc83c2f1774644620fa091f8712074
Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
2021-06-21 14:40:56 -0500

    BACKPORT: ASoC: Intel: use MODULE_DEVICE_TABLE with platform_device_id tables
    When we have a platform_device_id table, we can use
    MODULE_DEVICE_TABLE to automatically generate the modalias. As a
    result we can remove the manual insertion of MODULE_ALIAS.
    
    Reported-by: Hulk Robot <hulkci@huawei.com>
    Suggested-by: Zou Wei <zou_wei@huawei.com>
    Signed-off-by: Pierre-Louis Bossart <pierre-louis.bossart@linux.intel.com>
    Reviewed-by: Guennadi Liakhovetski <guennadi.liakhovetski@linux.intel.com>
    Link: https://lore.kernel.org/r/20210621194057.21711-10-pierre-louis.bossart@linux.intel.com
    Signed-off-by: Mark Brown <broonie@kernel.org>
    
    (cherry picked from commit 53b98536fb64f1b6ff5a1b2cfc36bbfa90619414)
    
    Conflicts:
       sound/soc/intel/boards/sof_rt5682.c
    
    BUG=b:188623237
    TEST=ODM tests audio function on Cret
    
    Signed-off-by: Brent Lu <brent.lu@intel.com>
    Change-Id: I7600dc610d10a47be8e4498a8e49f3761f66ec94
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3017347
    Tested-by: Ajye Huang <ajye_huang@compal.corp-partner.google.com>
    Reviewed-by: Shou-Chieh Hsu <shouchieh@chromium.org>
    Commit-Queue: Shou-Chieh Hsu <shouchieh@chromium.org>

--- sound/soc/intel/boards/kbl_rt5663_rt5514_max98927.c
+++ sound/soc/intel/boards/kbl_rt5663_rt5514_max98927.c
@@ -839,6 +839,7 @@ static const struct platform_device_id kbl_board_ids[] = {
 	{ .name = "kbl_r5514_5663_max" },
 	{ }
 };
+MODULE_DEVICE_TABLE(platform, kbl_board_ids);
 
 static struct platform_driver kabylake_audio = {
 	.probe = kabylake_audio_probe,
@@ -855,4 +856,3 @@ module_platform_driver(kabylake_audio)
 MODULE_DESCRIPTION("Audio Machine driver-RT5663 RT5514 & MAX98927");
 MODULE_AUTHOR("Harsha Priya <harshapriya.n@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_ALIAS("platform:kbl_r5514_5663_max");
