fdd24b49114408faca58e8525d4c673ef7048ea7
Guenter Roeck <groeck@google.com>
2021-10-15 08:51:54 -0700

    Revert "NOUPSTREAM: ANDROID: mm: Memory Management Exports"
    This reverts commit db400d94a2f57ef9374cffda2226c89b14f14e2c.
    
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- mm/cma.c
+++ mm/cma.c
@@ -24,7 +24,6 @@
 #include <linux/memblock.h>
 #include <linux/err.h>
 #include <linux/mm.h>
-#include <linux/module.h>
 #include <linux/sizes.h>
 #include <linux/slab.h>
 #include <linux/log2.h>
@@ -55,7 +54,6 @@ const char *cma_get_name(const struct cma *cma)
 {
 	return cma->name;
 }
-EXPORT_SYMBOL_GPL(cma_get_name);
 
 static unsigned long cma_bitmap_aligned_mask(const struct cma *cma,
 					     unsigned int align_order)
@@ -549,7 +547,6 @@ out:
 
 	return page;
 }
-EXPORT_SYMBOL_GPL(cma_alloc);
 
 /**
  * cma_release() - release allocated pages
@@ -584,7 +581,6 @@ bool cma_release(struct cma *cma, const struct page *pages,
 
 	return true;
 }
-EXPORT_SYMBOL_GPL(cma_release);
 
 int cma_for_each_area(int (*it)(struct cma *cma, void *data), void *data)
 {
@@ -599,4 +595,3 @@ int cma_for_each_area(int (*it)(struct cma *cma, void *data), void *data)
 
 	return 0;
 }
-EXPORT_SYMBOL_GPL(cma_for_each_area);
