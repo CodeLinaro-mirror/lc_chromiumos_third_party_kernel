603379cbd00023b31686b3840ed264f9a2f9a716
Guenter Roeck <groeck@google.com>
2021-10-15 08:52:01 -0700

    Revert "NOUPSTREAM: ANDROID: scsi: SCSI related vendor hooks"
    This reverts commit 884f8abb247c8888ce52eca32b5866d01ed64e26.
    
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- drivers/scsi/ufs/ufs-sysfs.c
+++ drivers/scsi/ufs/ufs-sysfs.c
@@ -9,8 +9,6 @@
 #include "ufs.h"
 #include "ufs-sysfs.h"
 
-#include <trace/hooks/ufshcd.h>
-
 static const char *ufshcd_uic_link_state_to_string(
 			enum uic_link_state state)
 {
@@ -1252,19 +1250,15 @@ const struct attribute_group ufs_sysfs_lun_attributes_group = {
 	.attrs = ufs_sysfs_lun_attributes,
 };
 
-void ufs_sysfs_add_nodes(struct ufs_hba *hba)
+void ufs_sysfs_add_nodes(struct device *dev)
 {
 	int ret;
 
-	ret = sysfs_create_groups(&hba->dev->kobj, ufs_sysfs_groups);
-	if (ret) {
-		dev_err(hba->dev,
+	ret = sysfs_create_groups(&dev->kobj, ufs_sysfs_groups);
+	if (ret)
+		dev_err(dev,
 			"%s: sysfs groups creation failed (err = %d)\n",
 			__func__, ret);
-		return;
-	}
-
-	trace_android_vh_ufs_update_sysfs(hba);
 }
 
 void ufs_sysfs_remove_nodes(struct device *dev)
