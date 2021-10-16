23d89f8a70ea539def86f191776cb4e9aca05edf
Guenter Roeck <groeck@google.com>
2021-10-15 17:08:07 -0700

    Revert "CHROMIUM: drm/evdi: Fix compilation in evdi_painter"
    This reverts commit 810b2d12b493e4ae9a8103ccddf7595e2ed67da6.
    
    Signed-off-by: Guenter Roeck <groeck@google.com>

--- drivers/gpu/drm/evdi/evdi_painter.c
+++ drivers/gpu/drm/evdi/evdi_painter.c
@@ -733,16 +733,12 @@ void evdi_painter_crtc_state_notify(struct evdi_device *evdi, int state)
 	}
 }
 
-static void evdi_log_pixel_format(uint32_t pixel_format, char *buf, size_t size)
+static void evdi_log_pixel_format(uint32_t pixel_format)
 {
-#if KERNEL_VERSION(5, 10, 0) <= LINUX_VERSION_CODE
-	snprintf(buf, size, "pixel format %p4cc", &pixel_format);
-#else
 	struct drm_format_name_buf format_name;
 
 	drm_get_format_name(pixel_format, &format_name);
-	snprintf(buf, size, "pixel format %s", format_name.str);
-#endif
+	EVDI_DEBUG("pixel format %s\n", format_name.str);
 }
 
 void evdi_painter_mode_changed_notify(struct evdi_device *evdi,
@@ -752,7 +748,6 @@ void evdi_painter_mode_changed_notify(struct evdi_device *evdi,
 	struct drm_framebuffer *fb;
 	int bits_per_pixel;
 	uint32_t pixel_format;
-	char buf[100];
 
 	if (painter == NULL)
 		return;
@@ -764,10 +759,10 @@ void evdi_painter_mode_changed_notify(struct evdi_device *evdi,
 	bits_per_pixel = fb->format->cpp[0] * 8;
 	pixel_format = fb->format->format;
 
-	evdi_log_pixel_format(pixel_format, buf, sizeof(buf));
-	EVDI_INFO("(card%d) Notifying mode changed: %dx%d@%d; bpp %d; %s",
-		  evdi->dev_index, new_mode->hdisplay, new_mode->vdisplay,
-		  drm_mode_vrefresh(new_mode), bits_per_pixel, buf);
+	EVDI_DEBUG("(dev=%d) Notifying mode changed: %dx%d@%d; bpp %d; ",
+		   evdi->dev_index, new_mode->hdisplay, new_mode->vdisplay,
+		   drm_mode_vrefresh(new_mode), bits_per_pixel);
+	evdi_log_pixel_format(pixel_format);
 
 	evdi_painter_send_mode_changed(painter,
 				       new_mode,
