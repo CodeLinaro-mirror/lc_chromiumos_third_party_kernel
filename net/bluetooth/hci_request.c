Atom-X-Devs/scarlet_xiaomi_sdm660

Revert "UPSTREAM: Bluetooth: Add support for utilizing Fast Advertising Interval"
This reverts commit 69fd484ffc300ffe78156646545cf6d99225720e.

Reason for revert: The more aggressive advertising intervals configured
in this CL causes instability on kukui. Reverting back to the previous
defaults resolve the issue.

Failure that is corrected:
< HCI Command: LE Set Advertising Parameters (0x08|0x0006) plen 15
Min advertising interval: 62.500 msec (0x0064)
        Max advertising interval: 93.750 msec (0x0096)
        Type: Non connectable undirected - ADV_NONCONN_IND (0x03)
        Own address type: Random (0x01)
        Direct address type: Public (0x00)
        Direct address: 00:00:00:00:00:00 (OUI 00-00-00)
        Channel map: 37, 38, 39 (0x07)
        Filter policy: Allow Scan Request from Any,
	        Allow Connect Request from Any (0x00)
> HCI Event: Command Complete (0x0e) plen 4
      LE Set Advertising Parameters (0x08|0x0006) ncmd 1
        Status: Invalid HCI Command Parameters (0x12)

Conflict was due to some new interval logic in Cros

Signed-off-by: Daniel Winkler <danielwinkler@google.com>

BUG=b:173797439
TEST=Can successfully register "broadcast" advertisements on kukui

Change-Id: I73b514cd269c3f48b7e8d57ea582c2f2f1364385
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2552216
Reviewed-by: Miao-chen Chou <mcchou@chromium.org>
Tested-by: Daniel Winkler <danielwinkler@google.com>
Commit-Queue: Daniel Winkler <danielwinkler@google.com>

net/bluetooth/hci_request.c
@@ -1504,23 +1504,16 @@ void __hci_req_enable_advertising(struct hci_request *req)
 		adv_max_interval = hdev->le_adv_max_interval;
 	}
 
-	if (connectable) {
-		cp.type = LE_ADV_IND;
-	} else {
-		if (get_cur_adv_instance_scan_rsp_len(hdev))
-			cp.type = LE_ADV_SCAN_IND;
-		else
-			cp.type = LE_ADV_NONCONN_IND;
-
-		if (!hci_dev_test_flag(hdev, HCI_DISCOVERABLE) ||
-		    hci_dev_test_flag(hdev, HCI_LIMITED_DISCOVERABLE)) {
-			adv_min_interval = DISCOV_LE_FAST_ADV_INT_MIN;
-			adv_max_interval = DISCOV_LE_FAST_ADV_INT_MAX;
-		}
-	}
-
 	cp.min_interval = cpu_to_le16(adv_min_interval);
 	cp.max_interval = cpu_to_le16(adv_max_interval);
+
+	if (connectable)
+		cp.type = LE_ADV_IND;
+	else if (get_cur_adv_instance_scan_rsp_len(hdev))
+		cp.type = LE_ADV_SCAN_IND;
+	else
+		cp.type = LE_ADV_NONCONN_IND;
+
 	cp.own_address_type = own_addr_type;
 	cp.channel_map = hdev->le_adv_channel_map;
 

CodeLinaro-mirror/lc_chromiumos_third_party_kernel

(cherry picked from commit c25deed4ee12d860ee5cad3abc20157a40672020)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2595668
Reviewed-by: Alain Michaud <alainm@chromium.org>
Change-Id: Ie8f554d98916bb83cd585a58068b813ecf97a6b9
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/2780554

net/bluetooth/hci_request.c
@@ -1595,23 +1595,16 @@ void __hci_req_enable_advertising(struct hci_request *req)
 		adv_max_interval = hdev->le_adv_max_interval;
 	}
 
-	if (connectable) {
-		cp.type = LE_ADV_IND;
-	} else {
-		if (adv_cur_instance_is_scannable(hdev))
-			cp.type = LE_ADV_SCAN_IND;
-		else
-			cp.type = LE_ADV_NONCONN_IND;
-
-		if (!hci_dev_test_flag(hdev, HCI_DISCOVERABLE) ||
-		    hci_dev_test_flag(hdev, HCI_LIMITED_DISCOVERABLE)) {
-			adv_min_interval = DISCOV_LE_FAST_ADV_INT_MIN;
-			adv_max_interval = DISCOV_LE_FAST_ADV_INT_MAX;
-		}
-	}
-
 	cp.min_interval = cpu_to_le16(adv_min_interval);
 	cp.max_interval = cpu_to_le16(adv_max_interval);
+
+	if (connectable)
+		cp.type = LE_ADV_IND;
+	else if (adv_cur_instance_is_scannable(hdev))
+		cp.type = LE_ADV_SCAN_IND;
+	else
+		cp.type = LE_ADV_NONCONN_IND;
+
 	cp.own_address_type = own_addr_type;
 	cp.channel_map = hdev->le_adv_channel_map;
 