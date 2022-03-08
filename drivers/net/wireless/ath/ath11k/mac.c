329f2e438f03de8821f43d5d20c7e54460b3394f
sampnimm <quic_sampnimm@quicinc.com>
2022-03-08 16:56:14 +0530

    INTERNAL: ath11k: Add support to connect to non-transmit
    MBSSID profile
    
    Change-Id: Ifda1ee1a895b1c3b417bd7f228351d696dabfc60
    Signed-off-by: sampnimm <quic_sampnimm@quicinc.com>

--- drivers/net/wireless/ath/ath11k/mac.c
+++ drivers/net/wireless/ath/ath11k/mac.c
@@ -8988,8 +8988,12 @@ static int __ath11k_mac_register(struct ath11k *ar)
 
 	ar->hw->wiphy->interface_modes = ab->hw_params.interface_modes;
 
-	if (ab->hw_params.single_pdev_only && ar->supports_6ghz)
-		ieee80211_hw_set(ar->hw, SINGLE_SCAN_ON_ALL_BANDS);
+	if (ab->hw_params.single_pdev_only) {
+		ieee80211_hw_set(ar->hw, SUPPORTS_MULTI_BSSID);
+		ieee80211_hw_set(ar->hw, SUPPORTS_ONLY_HE_MULTI_BSSID);
+		if (ar->supports_6ghz)
+			ieee80211_hw_set(ar->hw, SINGLE_SCAN_ON_ALL_BANDS);
+	}
 
 	ieee80211_hw_set(ar->hw, SIGNAL_DBM);
 	ieee80211_hw_set(ar->hw, SUPPORTS_PS);
